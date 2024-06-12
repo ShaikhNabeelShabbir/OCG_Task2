6fb8c94e5d38251d908b92c2e98c9d374b42b14c
Shaikh Nabeel Shabbir <98305180+ShaikhNabeelShabbir@users.noreply.github.com>
2024-06-12 14:43:19 +0530

    Update index.ts

--- index.ts
+++ index.ts
@@ -1,6 +1,6 @@
 import { promises as fs } from "fs";
-//Function used to fetch data from the api
-async function fetchDATA(url: string, name: string) {
+
+async function fetchData(url: string, fileName: string) {
   console.log("Fetching data");
   try {
     const response = await fetch(url);
@@ -8,9 +8,8 @@ async function fetchDATA(url: string, name: string) {
       throw new Error("Data not found");
     }
     const data = await response.json();
-    //Processing the data by converting to string
-    const JData = JSON.stringify(data);
-    await JSON_File_Creator(JData, name);
+    const jsonData = JSON.stringify(data);
+    await createJsonFile(jsonData, fileName);
     return data;
   } catch (error) {
     console.error(error);
@@ -18,76 +17,66 @@ async function fetchDATA(url: string, name: string) {
   }
 }
 
-//Function used to create a JSON file
-const JSON_File_Creator = async (
-  json_string: string,
-  name: string
-): Promise<void> => {
+const createJsonFile = async (jsonString: string, fileName: string) => {
   console.log("Creating JSON file");
-  // Check if file already exists
   try {
-    await fs.writeFile(`${name}.json`, json_string);
+    await fs.writeFile(`${fileName}.json`, jsonString);
     console.log("Data written to file");
   } catch (err) {
     console.error("Error writing file", err);
   }
 };
 
-//ask about any and file types that
-// function used to find the top ten records
-const firstTen = async (file: any) => {
+const getFirstTenRecords = async (file: any) => {
   console.log("Fetching first 10 records");
   try {
     const fileContent = await fs.readFile(file, "utf8");
     const data = JSON.parse(fileContent);
-    var top10 = data.slice(0, 10);
-    console.log(top10);
+    const topTen = data.slice(0, 10);
+    console.log(topTen);
+    return topTen;
   } catch (err) {
     console.error("Error reading or parsing file", err);
+    return [];
   }
-  return top10;
 };
 
-// function parse over the given json string
-const parsingJSON = async (top10: Array<string>) => {
+const parseJson = async (topTen: Array<string>) => {
   let result: { [key: string]: number } = {};
   try {
-    // Read the JSON file
     const data = await fs.readFile("coingecko_vs_currencies.json", "utf8");
-    // Parse the JSON file
     const obj = JSON.parse(data.toString());
     const list = obj.rates;
-    //Iterate over the JSON object
-    for (let index = 0; index < top10.length; index++) {
-      const element = top10[index];
+    for (let index = 0; index < topTen.length; index++) {
+      const element = topTen[index];
       if (element in list) {
         const x = 1 / list[element].value;
         console.log(`${element}: ${x} bitcoin`);
         result[element] = x;
       } else {
-        console.log("Not Happen");
+        console.log("Not Found");
       }
     }
   } catch (err) {
     console.error("Error reading or parsing file", err);
   }
-  const jResult = JSON.stringify(result, null, 2);
-  JSON_File_Creator(jResult, "result");
+  const jsonResult = JSON.stringify(result, null, 2);
+  createJsonFile(jsonResult, "result");
 };
 
-// Main function to fetch data and process it
 async function main() {
-  await fetchDATA(
+  await fetchData(
     "https://api.coingecko.com/api/v3/simple/supported_vs_currencies",
     "supported_vs_currencies"
   );
-  const top10 = await firstTen("supported_vs_currencies.json");
+  const topTen = await getFirstTenRecords("supported_vs_currencies.json");
 
-  await fetchDATA(
+  await fetchData(
     "https://api.coingecko.com/api/v3/exchange_rates",
     "coingecko_vs_currencies"
   );
 
-  parsingJSON(top10);
+  await parseJson(topTen);
 }
+
 main();
